de921db9d9f69489efb2bc0d10c55503f67cf418
Robin Visser <70282027+Robinzor@users.noreply.github.com>
2023-05-16 21:47:12 +0200

    Update main.py

--- boefjes/boefjes/plugins/kat_rdp/main.py
+++ boefjes/boefjes/plugins/kat_rdp/main.py
@@ -3,7 +3,7 @@ from typing import List, Tuple, Union
 import socket
 import ssl
 from cryptography import x509
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.backends import default_backend c
 from cryptography.hazmat.primitives.serialization import PublicFormat, Encoding
 import datetime
 import json
@@ -18,49 +18,38 @@ def run_rdp(args: List[str]) -> str:
 
     # Test SSL/TLS verions
     context.verify_mode = ssl.CERT_NONE
-    ssl_disabled = False
 
-    try:
-        with socket.create_connection((args, port)) as sock:
-            with context.wrap_socket(sock, server_hostname=args) as ssock:
-                print("[!]", ssock.version(),"is enabled")
-    except ConnectionRefusedError:
-        print("SSL is not enabled")
-        ssl_disabled = True
+    detection_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    cert = ssl.get_server_certificate((args, port))
+    x509_cert = x509.load_pem_x509_certificate(cert.encode(), default_backend())
+    if x509_cert.issuer == x509_cert.subject:
+        print("[!] The certificate is self-signed.")
+    else:
+        print("The certificate is not self-signed.")
 
-    # Test if certificate is selfsigned.
-    if ssl_disabled == False:
-        detection_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        
-        cert = ssl.get_server_certificate((args, port))
-        x509_cert = x509.load_pem_x509_certificate(cert.encode(), default_backend())
-        if x509_cert.issuer == x509_cert.subject:
-            print("[!] The certificate is self-signed.")
-        else:
-            print("The certificate is not self-signed.")
-
-        data = {
-                "address": args, 
-                "time": detection_time,
-                "ssl_enabled": ssl_disabled,
-                "version": x509_cert.version,
-                "serialnumber":hex(x509_cert.serial_number)[2:],
-                "sig":x509_cert.signature_hash_algorithm.name,
-                "issuer":x509_cert.issuer.rfc4514_string(),
-                "validfrom":x509_cert.not_valid_before,
-                "validuntil":x509_cert.not_valid_after,
-                "subject":x509_cert.subject.rfc4514_string(),
-                "pubhex":x509_cert.public_key().public_bytes(
-        encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo).hex(),
-                "pubdata":x509_cert.public_key().public_bytes(
-        encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo).decode(),
-                }
-        return data
+    data = {
+            "address": args, 
+            "time": detection_time,
+            "ssl_enabled": ssl_disabled,
+            "version": x509_cert.version,
+            "serialnumber":hex(x509_cert.serial_number)[2:],
+            "sig":x509_cert.signature_hash_algorithm.name,
+            "issuer":x509_cert.issuer.rfc4514_string(),
+            "validfrom":x509_cert.not_valid_before,
+            "validuntil":x509_cert.not_valid_after,
+            "subject":x509_cert.subject.rfc4514_string(),
+            "pubhex":x509_cert.public_key().public_bytes(
+    encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo).hex(),
+            "pubdata":x509_cert.public_key().public_bytes(
+    encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo).decode(),
+            }
+    return data
 
 
 def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
     """return results to normalizer."""
-    input_ = boefje_meta.arguments["input"]
-    ip_address = input_["address"]
-    results = run_rdp(ip_address)
+    args = boefje_meta.arguments["input"]
+    results = run_rdp(args)
     return [(set(), json.dumps(results))]
+
