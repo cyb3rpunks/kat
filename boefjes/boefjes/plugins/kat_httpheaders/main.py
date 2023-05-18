35fea60e3147e268cb75c3328bda930ed5cf4dbe
Robin Visser <robinvisser92@live.nl>
2023-05-18 13:45:58 +0200

    uploading cornet code

---
+++ boefjes/boefjes/plugins/kat_httpheaders/main.py
@@ -0,0 +1,27 @@
+import json
+import requests
+from requests.exceptions import Timeout
+from typing import List, Tuple, Union
+from boefjes.job_models import BoefjeMeta
+
+
+def run_httpheaders(url: List[str]) -> dict:
+    """Checks HTTPHeaders"""
+    # Create an SSH client
+    try:
+        response = requests.get(url, timeout=30)
+        headers = dict(response.headers)
+        headers_json = json.dumps(headers, indent=4).replace("'", '"')
+    except Timeout:
+        print("Timeout occurred while making the request.")
+        return headers_json
+
+def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
+    """return results to normalizer."""
+    input_ = boefje_meta.arguments["input"]
+    url = input_["address"]
+    results = run_httpheaders(url)
+    return [(set(), json.dumps(results))]
+
+
+
