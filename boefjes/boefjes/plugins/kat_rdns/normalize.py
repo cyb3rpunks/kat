967e925f14f61f9af48f9085abd2f1ef31e39398
Robin Visser <robinvisser92@live.nl>
2023-05-25 21:05:46 +0200

    Update normalize.py

--- boefjes/boefjes/plugins/kat_rdns/normalize.py
+++ boefjes/boefjes/plugins/kat_rdns/normalize.py
@@ -1,13 +1,12 @@
 import json
 from typing import Iterable, Union
-
 from boefjes.job_models import NormalizerMeta
 from boefjes.job_models import BoefjeMeta
 from octopoes.models import OOI, Reference
-from octopoes.models.ooi.network import IPAddressV4, IPAddressV6, Network
 from octopoes.models.ooi.dns.records import (
     DNSPTRRecord,
 )
+
 
 def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
     ip_address = Reference.from_str(normalizer_meta.raw_data.boefje_meta.input_ooi)
